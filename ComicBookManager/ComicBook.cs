7c155cb580965f463a9dd3ded1b2bbbfde516a3f
James Hulme <jameshu@microsoft.com>
2020-01-21 10:09:32 -0800

    Added classes

---
+++ ComicBookManager/ComicBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicBookManager
+{
+    class ComicBook
+    {
+        #region Properties
+        public string BookTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public DateTime Date { get; set; }
+        public decimal CoverPrice { get; set; }
+        public string CoverArtist { get; set; }
+        public string StoryTitle { get; set; }
+        public string Note { get; set; }
+        public List<string> Writers { get; set; }
+        public List<string> Pencillers { get; set; }
+        public List<string> Inkers { get; set; }
+        public List<string> Colorists { get; set; }
+        public List<string> Characters { get; set; }
+        #endregion
+    }
+}
