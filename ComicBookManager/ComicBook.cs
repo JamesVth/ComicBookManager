c853c14acb71435a75e0bfa07e466cbe60c726da
James Hulme <jameshu@microsoft.com>
2020-01-28 09:56:13 -0800

    Added constructor and methods to add artists.

--- ComicBookManager/ComicBook.cs
+++ ComicBookManager/ComicBook.cs
@@ -4,21 +4,67 @@ using System.Text;
 
 namespace ComicBookManager
 {
+    enum BasicGrade { Near_Mint, Verry_Fine, Fine, Very_Good, Good, Fair, Poor }
     class ComicBook
     {
         #region Properties
+        public string Publisher { get; set; }
         public string BookTitle { get; set; }
         public int IssueNumber { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime PublishedDate { get; set; }
         public decimal CoverPrice { get; set; }
         public string CoverArtist { get; set; }
         public string StoryTitle { get; set; }
         public string Note { get; set; }
-        public List<string> Writers { get; set; }
-        public List<string> Pencillers { get; set; }
-        public List<string> Inkers { get; set; }
-        public List<string> Colorists { get; set; }
-        public List<string> Characters { get; set; }
+        public List<string> Writers { get; private set; }
+        public List<string> Pencillers { get; private set; }
+        public List<string> Inkers { get; private set; }
+        public List<string> Colorists { get; private set; }
+        public List<string> Characters { get; private set; }
+        #endregion
+
+        #region Methods
+        public void AddWriter(string Writer)
+        {
+            Writers.Add(Writer);
+        }
+
+        public void AddPenciller(string Penciller)
+        { 
+            Pencillers.Add(Penciller);
+        }
+
+        public void AddInker(string Inker)
+        {
+            Inkers.Add(Inker);
+        }
+
+        public void AddColorist(string Colorist)
+        {
+            Colorists.Add(Colorist);
+        }
+
+        public void AddCharacter(string Character)
+        {
+            Characters.Add(Character);
+        }
+        #endregion
+
+        #region Contructors
+        public ComicBook(string Publisher, string BookTitle, int IssueNumber, DateTime PublishedDate, Decimal CoverPrice)
+        {
+            var Writers = new List<string>();
+            var Pencillers = new List<string>();
+            var Inkers = new List<string>();
+            var Colorists = new List<string>();
+            var Characters = new List<string>();
+
+            this.Publisher = Publisher;
+            this.BookTitle = BookTitle;
+            this.IssueNumber = IssueNumber;
+            this.PublishedDate = PublishedDate;
+            this.CoverPrice = CoverPrice;
+        }
         #endregion
     }
 }
--- ComicBookManager/Program.cs
+++ ComicBookManager/Program.cs
@@ -6,7 +6,23 @@ namespace ComicBookManager
     {
         static void Main(string[] args)
         {
-            
+            var MyComic = new ComicBook("DC", "Superman", 300, DateTime.Parse("1976-03-11"), 0.30m);
+            MyComic.StoryTitle = "Superman 2001";
+            MyComic.CoverArtist = "Bob Oksner";
+            MyComic.AddWriter("Cary Bates");
+            MyComic.AddWriter("Elliot S Maggin");
+            MyComic.AddPenciller("CurtSwan");
+            MyComic.AddInker("Bob Oksner");
+            MyComic.AddCharacter("Superman");
+            MyComic.AddCharacter("Jor-El");
+            MyComic.AddCharacter("Lara");
+            MyComic.AddCharacter("Lt. Thomas Clark");
+            MyComic.AddCharacter("Gen. Kent Garrett");
+            MyComic.AddCharacter("Premier Leonov");
+            MyComic.AddCharacter("Pres. Weiner");
+            MyComic.AddCharacter("Moka");
+
+            Console.WriteLine($"Comic Book: Publisher:{MyComic.Publisher}, Title:{MyComic.BookTitle}, Published:{MyComic.PublishedDate:yyyy/MM/dd}, CoverPrice:{MyComic.CoverPrice}");
         }
     }
 }
