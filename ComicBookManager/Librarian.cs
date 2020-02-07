b471a8fb20bb54f5500b96930e04aef010bd56c2
James Hulme <james2k@comcast.net>
2020-02-07 10:54:01 -0800

    Added the Librarian factory class and an AddComic method.

---
+++ ComicBookManager/Librarian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicBookManager
+{
+    static class Librarian
+    {
+        static ComicBook AddComic(string publisher, string bookTitle, int issueNumber, DateTime publishedDate, Decimal coverPrice)
+        {
+            var newcomic = new ComicBook(publisher, bookTitle, issueNumber, publishedDate, coverPrice);
+            return newcomic;
+        }
+    }
+}
